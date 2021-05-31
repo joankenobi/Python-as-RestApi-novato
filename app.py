a8ac1ef53371843ad42f2ffa4e12368030339e79
JOANB <joan1233blanco@gmail.com>
2021-05-31 16:01:23 -0400

    Hicimos que el servidor enviara un json

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # aplicacion de python, nuestro bakend, nuetra rest API
-#utilizamos flask como franword, pero existen muchos otros
-from flask import Flask
+#jsonify combierte un objeto al formato para navegador
+from flask import Flask ,jsonify
 
 #flask devolvera un objeto llamado app
 app=Flask(__name__) #app sera la aplicacion del servidor
@@ -9,11 +9,13 @@ app=Flask(__name__) #app sera la aplicacion del servidor
 
 from products import products
 #ruta de prueba para ver el funcionamiento del servidor
-@app.route("/ping") #cuando el navegador pida la ruta "/ping" respondera con:
+@app.route("/ping") #Las rutas permiten especificar los metodos HTTP(estos simplemente quieren decir que es lo que queremos hacer dentro del servidor)
 def ping(): #funcion ping que se ejecuta despues de pedir la ruta ping
-      return "pong!" #proceso de testeo, queremos responder Json
+      return jsonify({"mensaje":"pong!"}) #Un objeto json
 
-
+@app.route("/products", methods= ["GET"]) #tenemos otra ruta que funciona (trabaja) por el metodo http GET, por defecto las rutas funcionan con metodo GET
+def getProducts(): #funcion que retorna los productos like json
+    return jsonify(products) #ya aqui el cliente puede tomar los dato enviados por la Api
 
 if __name__ == "__main__": #si corre commo la principal
     app.run(debug=True, port=4000) #debug is for and 4000 is the port where escucha el servidor
